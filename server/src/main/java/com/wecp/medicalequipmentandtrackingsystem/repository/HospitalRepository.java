5e90e3a228b99321380010cbdab7c7c3b6597849
username <wecp-dummy@gmail.com>
2025-10-03 11:55:23 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/HospitalRepository.java
@@ -0,0 +1,12 @@
+package com.wecp.medicalequipmentandtrackingsystem.repository;
+
+
+import com.wecp.medicalequipmentandtrackingsystem.entitiy.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface HospitalRepository  {
+    // extends JpaRepositor and add custom methods if needed
+}
+
---
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/service/MaintenanceService.java
@@ -0,0 +1,17 @@
+package com.wecp.medicalequipmentandtrackingsystem.service;
+
+
+import com.wecp.medicalequipmentandtrackingsystem.entitiy.Equipment;
+import com.wecp.medicalequipmentandtrackingsystem.entitiy.Maintenance;
+import com.wecp.medicalequipmentandtrackingsystem.repository.EquipmentRepository;
+import com.wecp.medicalequipmentandtrackingsystem.repository.MaintenanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+
+public class MaintenanceService {
+    
+}
