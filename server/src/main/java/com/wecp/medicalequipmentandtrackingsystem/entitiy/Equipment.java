4de641fcf85548912f01f879be1a16b3b5714c5c
username <wecp-dummy@gmail.com>
2025-10-04 04:53:11 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/entitiy/Equipment.java
@@ -0,0 +1,9 @@
+package com.wecp.medicalequipmentandtrackingsystem.entitiy;
+
+
+import javax.persistence.*;
+
+@Table(name = "equipments") // do not change table name
+public class Equipment {
+
+}
---
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/MaintenanceRepository.java
@@ -0,0 +1,13 @@
+package com.wecp.medicalequipmentandtrackingsystem.repository;
+
+
+import com.wecp.medicalequipmentandtrackingsystem.entitiy.Maintenance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public interface MaintenanceRepository  {
+    // extend jpa repository and add custom methods if needed
+}
